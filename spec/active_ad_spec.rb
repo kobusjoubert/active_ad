9a63d45d9bee26dff41ea3261582753f3d73c5ca
Kobus Joubert <kobus51@yahoo.com>
2021-06-01 10:12:03 +0200

    chore: remove frozen_string_literal: true

--- spec/active_ad_spec.rb
+++ spec/active_ad_spec.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 RSpec.describe ActiveAd do
   it "has a version number" do
     expect(ActiveAd::VERSION).not_to be nil
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require "active_ad"
 
 RSpec.configure do |config|
