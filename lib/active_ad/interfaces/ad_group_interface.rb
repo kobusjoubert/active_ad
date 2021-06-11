231a14fdf6c6bea9ff06c033327290afefecf2e5
Kobus Joubert <kobus51@yahoo.com>
2021-06-11 07:13:19 +0200

    refactor: interface base class

--- lib/active_ad/interfaces/ad_group_interface.rb
+++ lib/active_ad/interfaces/ad_group_interface.rb
@@ -1,61 +1,8 @@
-class ActiveAd::AdGroupInterface
-  extend ActiveModel::Callbacks
-  include ActiveModel::Validations
-
-  attr_accessor :campaign, :response, :name
+class ActiveAd::AdGroupInterface < ActiveAd::BaseInterface
+  attr_accessor :campaign, :name
 
   validates_presence_of :campaign
 
-  define_model_callbacks :save, :update, :destroy
-
   # before_save :do_something
   # after_destroy :do_something
-
-  def initialize(**kwargs)
-    kwargs.each do |key, value|
-      send("#{key}=", value)
-    end
-  end
-
-  def save
-    self.response = nil
-
-    run_callbacks(:save) do
-      self.response = create_request
-    end
-
-    self
-  end
-
-  def update
-    self.response = nil
-
-    run_callbacks(:update) do
-      self.response = update_request
-    end
-
-    self
-  end
-
-  def destroy
-    self.response = nil
-
-    run_callbacks(:destroy) do
-      self.response = delete_request
-    end
-
-    self
-  end
-
-  def create_request
-    raise NotImplementedError
-  end
-
-  def update_request
-    raise NotImplementedError
-  end
-
-  def delete_request
-    raise NotImplementedError
-  end
 end
