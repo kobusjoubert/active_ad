51e845e40a08ec77c1c27f25d3eac501e5947e72
Kobus Joubert <kobus51@yahoo.com>
2021-07-01 06:11:06 +0200

    feat: limit method on relation object

--- lib/active_ad/relation.rb
+++ lib/active_ad/relation.rb
@@ -4,16 +4,19 @@ class ActiveAd::Relation
   # meaningful <=> operator, as these methods rely on an ordering between members of the collection.
   include Enumerable
 
-  attr_reader :klass, :kwargs, :index
+  attr_reader :klass, :kwargs
 
+  # Platforms using this approach: Facebook.
+  #
+  # This class uses the `limit` and `after` query parameters.
+  #
+  # === Example
+  #
+  #   https://somewhere.com/resources?limit=10&after=a1b2c3
   def initialize(klass, **kwargs)
     @klass = klass
     @kwargs = kwargs
-    @index ||= 0
-    # @client = klass.client
-    # @cursor
-    # @before
-    # @after
+    @limit = Float::INFINITY
     # model_type = klass.to_s.split('::').last.underscore.to_sym # :account, :campaign, :ad_group, :ad
   end
 
@@ -21,13 +24,15 @@ class ActiveAd::Relation
   #
   # {
   #   "data": [
-  #      ... Endpoint data is here
+  #      ...
   #   ],
   #   "paging": {
   #     "cursors": {
   #       "after": "MTAxNTExOTQ1MjAwNzI5NDE=",
   #       "before": "NDMyNzQyODI3OTQw"
-  #     }
+  #     },
+  #     "previous": "https://somewhere.com/resources?limit=25&before=NDMyNzQyODI3OTQw", # Only present if it exists.
+  #     "next": "https://somewhere.com/resources?limit=25&after=MTAxNTExOTQ1MjAwNzI5NDE=" # Only present if it exists.
   #   }
   # }
   #
@@ -35,9 +40,7 @@ class ActiveAd::Relation
   #
   # {
   #   "error": {
-  #     "message": "(#100) The After Cursor specified exceeds the max limit supported by this endpoint",
-  #     "type": "OAuthException",
-  #     "code": 100
+  #     "message": "no no no"
   #   }
   # }
   #
@@ -45,8 +48,12 @@ class ActiveAd::Relation
   def each
     return to_enum(:each) unless block_given?
 
+    index = 0
+    total = 0
+
     loop do
       p "=== index: #{index}"
+      break if total >= @limit
 
       raise index_response.body['error'] unless index_response.success?
 
@@ -56,24 +63,32 @@ class ActiveAd::Relation
         object = klass.new(**attributes)
         yield object
       else
-        @index = 0
+        index = 0
         after = index_response.body.dig('paging', 'cursors', 'after')
         break unless after
 
         @_index_response = klass.index_request(after: after, **kwargs)
       end
 
-      @index += 1
+      index += 1
+      total += 1
     end
   end
 
-  # TODO: Implement some sort of count_request method.
+  def limit(value)
+    @limit = value.to_i
+    self
+  end
+
+  # TODO: Implement a more efficient count_request method.
   def count
     super
   end
 
-  # Invalidate cache when reaching end of list and paginating over to the next set of results.
+  # Invalidate cache when reaching the end of the current list and paginating over to the next set of results. Also enforce a `limit` parameter to the API
+  # calls when supplied.
   def index_response
+    kwargs[:limit] = @limit unless @limit.infinite?
     @_index_response ||= klass.index_request(**kwargs)
   end
 end
