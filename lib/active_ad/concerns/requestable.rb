3c87a2c4b33fe0e28037794c59e934849056ef74
Kobus Joubert <kobus51@yahoo.com>
2021-07-07 10:19:39 +0200

    feat: ActiveAd::Requestable to log requests, and disallow any unsupported request methods

--- lib/active_ad/concerns/requestable.rb
+++ lib/active_ad/concerns/requestable.rb
@@ -1,9 +1,18 @@
 module ActiveAd::Requestable
   include ActiveSupport::Concern
 
+  REQUEST_METHODS = %i[get head delete trace post put patch]
+
   def request(kwargs)
-    request_method = kwargs.keys[0] # :get, :head, :delete, :trace, :post, :put, :patch
+    request_method = kwargs.keys[0]
+
+    unless REQUEST_METHODS.include?(request_method)
+      raise ArgumentError, "First key in the arguments hash was #{request_method}, must be one of #{REQUEST_METHODS.join(', ')}"
+    end
+
     url = kwargs.delete(request_method)
+
+    ActiveAd.logger.info("Sending #{request_method.upcase} request to #{url} with kwargs: #{kwargs}")
 
     ActiveAd.connection.send(request_method, url) do |req|
       req.headers = kwargs[:headers] if kwargs[:headers]
