9f91ee78e5c088a620d7c651c9d2a356ba1a791c
Kobus Joubert <kobus51@yahoo.com>
2021-07-09 13:00:37 +0200

    chore: titles might have to move from Ad -> AdCreative

--- lib/active_ad/ad.rb
+++ lib/active_ad/ad.rb
@@ -13,9 +13,11 @@ class ActiveAd::Ad < ActiveAd::Base
   # delegate :platform, :api_version, :access_token, to: :ad_group
 
   # validates_presence_of :some_attribute
-  validates_presence_of :title_or_titles, on: :create
-  validates_length_of :title, maximum: 255, unless: :platform_checks_length_of_title, allow_blank: true
-  validates :titles, titles_length: { maximums: [255] }, unless: :platform_checks_length_of_titles, allow_blank: true
+
+  # TODO: These 3 might need to move to AdCreative. Will this play nice with other platforms?
+  # validates_presence_of :title_or_titles, on: :create
+  # validates_length_of :title, maximum: 255, unless: :platform_checks_length_of_title, allow_blank: true
+  # validates :titles, titles_length: { maximums: [255] }, unless: :platform_checks_length_of_titles, allow_blank: true
 
   # before_save :do_something
   # after_destroy :do_something
