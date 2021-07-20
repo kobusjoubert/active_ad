61e868337ec6bd881e86d93f925d53f97ce54e85
Kobus Joubert <kobus51@yahoo.com>
2021-07-20 07:30:25 +0200

    feat: attributes for Facebook account

--- lib/active_ad/clients/facebook/account.rb
+++ lib/active_ad/clients/facebook/account.rb
@@ -1,44 +1,100 @@
 class ActiveAd::Facebook::Account < ActiveAd::Base
-  # Requesting `direct_deals_tos_accepted` causes a status `400` with message `(#3) Ad Account must be on allowlist`.
+  # Requesting the following fields causes status `400` error with messages.
   #
-  # The `id` field returnes a `act_` prefixed value, possibly a Facebook hack because they needed the values to be globally unique. When you look at things
-  # like `campaign.account_id` it doesn't return the `act_` prefixed value. So whenever the `id` attribute is being set on an object, we remove the prefixed
-  # `act_` value.
+  #   'direct_deals_tos_accepted' => "(#3) Ad Account must be on allowlist".
+  #   'has_page_authorized_adaccount' => "(#100) For field 'has_page_authorized_adaccount': The parameter page_id is required".
+  #   'show_checkout_experience' => "(#100) For field 'show_checkout_experience': The parameter page_id is required".
+  #
+  # The 'id' field returnes an 'act_' prefixed value, possibly a Facebook hack because they might have needed values to be globally unique ¯\_(ツ)_/¯. When you
+  # look at things like 'campaign.account_id' it doesn't return the 'act_' prefixed value. So whenever the 'id' attribute is being set on an object, we remove
+  # the prefixed 'act_' value.
   #
   #   'id' => 'act_123'
   #   'account_id' => '123'
   READ_FIELDS = %i[
-    account_id account_status age amount_spent balance business_city business_country_code business_name business_state business_street business_street2
-    business_zip capabilities created_time currency disable_reason end_advertiser end_advertiser_name failed_delivery_checks funding_source
-    funding_source_details id is_direct_deals_enabled is_notifications_enabled is_personal is_prepay_account media_agency min_campaign_group_spend_cap
-    min_daily_budget name offsite_pixels_tos_accepted owner partner rf_spec spend_cap timezone_id timezone_name timezone_offset_hours_utc tos_accepted
-    user_tos_accepted
+    account_id account_status age agency_client_declaration amount_spent balance business business_city business_country_code business_name business_state
+    business_street business_street2 business_zip can_create_brand_lift_study capabilities created_time currency disable_reason end_advertiser
+    end_advertiser_name extended_credit_invoice_group failed_delivery_checks fb_entity funding_source funding_source_details has_migrated_permissions id
+    io_number is_attribution_spec_system_default is_direct_deals_enabled is_in_3ds_authorization_enabled_market is_in_middle_of_local_entity_migration
+    is_notifications_enabled is_personal is_prepay_account is_tax_id_required line_numbers media_agency min_campaign_group_spend_cap min_daily_budget name
+    offsite_pixels_tos_accepted owner partner rf_spec spend_cap tax_id tax_id_status tax_id_type timezone_id timezone_name timezone_offset_hours_utc
+    tos_accepted user_tasks user_tos_accepted
   ].freeze
 
   has_many :campaigns
 
-  # Identification attributes.
-  attribute :account_id, :string
-
-  # Titles and descriptions attributes.
-  attribute :name, :string
-
-  # Other attributes.
-  attribute :business_id, :string
-  attribute :currency, :string
-  attribute :end_advertiser_id, :string
-  attribute :media_agency_id, :string
-  attribute :partner_id, :string
-  attribute :status, :string
-  attribute :timezone_id, :integer
-
-  # Use aliases to map external API attributes to the object attributes.
+  # Use aliases to map external API attributes to the ActiveAd object attributes. We especially want to make sure identitfication attributes end with an '_id'
+  # suffix. For example 'platform_attribute' should be aliased as 'platform_attribute_id'. This way when we call 'object.platform_attribute_id' we know we're
+  # getting back an ID instead of an object.
   #
   # alias_attribute :platform_attribute, :active_ad_attribute
+  alias_attribute :account_id, :id
   alias_attribute :account_status, :status
   alias_attribute :end_advertiser, :end_advertiser_id
+  alias_attribute :funding_source, :funding_source_id
   alias_attribute :media_agency, :media_agency_id
+  alias_attribute :owner, :owner_id
   alias_attribute :partner, :partner_id
+
+  # ActiveAd object attributes.
+  attribute :ad_account_promotable_objects
+  attribute :age, :float
+  attribute :agency_client_declaration
+  attribute :amount_spent, :string
+  attribute :balance, :string
+  attribute :business
+  attribute :business_city, :string
+  attribute :business_country_code, :string
+  attribute :business_name, :string
+  attribute :business_state, :string
+  attribute :business_street, :string
+  attribute :business_street2, :string
+  attribute :business_zip, :string
+  attribute :can_create_brand_lift_study, :boolean
+  attribute :capabilities
+  attribute :created_time, :datetime
+  attribute :currency, :string
+  attribute :direct_deals_tos_accepted, :boolean
+  attribute :disable_reason, :integer
+  attribute :end_advertiser_id, :string
+  attribute :end_advertiser_name, :string
+  attribute :extended_credit_invoice_group
+  attribute :failed_delivery_checks
+  attribute :fb_entity, :integer
+  attribute :funding_source_id, :string
+  attribute :funding_source_details
+  attribute :has_migrated_permissions, :boolean
+  attribute :has_page_authorized_adaccount, :boolean
+  attribute :io_number, :string
+  attribute :is_attribution_spec_system_default, :boolean
+  attribute :is_direct_deals_enabled, :boolean
+  attribute :is_in_3ds_authorization_enabled_market, :boolean
+  attribute :is_in_middle_of_local_entity_migration, :boolean
+  attribute :is_notifications_enabled, :boolean
+  attribute :is_personal, :integer
+  attribute :is_prepay_account, :boolean
+  attribute :is_tax_id_required, :boolean
+  attribute :line_numbers
+  attribute :media_agency_id, :string
+  attribute :min_campaign_group_spend_cap, :string
+  attribute :min_daily_budget, :integer
+  attribute :name, :string
+  attribute :offsite_pixels_tos_accepted, :boolean
+  attribute :owner_id, :string
+  attribute :partner_id, :string
+  attribute :rf_spec
+  attribute :show_checkout_experience, :boolean
+  attribute :spend_cap, :string
+  attribute :status, :integer
+  attribute :tax_id, :string
+  attribute :tax_id_status, :integer
+  attribute :tax_id_type, :string
+  attribute :timezone_id, :integer
+  attribute :timezone_name, :string
+  attribute :timezone_offset_hours_utc, :float
+  attribute :tos_accepted
+  attribute :user_tasks
+  attribute :user_tos_accepted
 
   # Use validations which will overwrite the parent class implementations.
   #
