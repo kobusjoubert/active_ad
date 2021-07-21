78e0f6ec7c5749699097ba8b631632c160149ae5
Kobus Joubert <kobus51@yahoo.com>
2021-07-21 13:07:23 +0200

    refactor: all date attributes to end with "_at" and "id" attribute as first in list

--- lib/active_ad/clients/facebook/campaign.rb
+++ lib/active_ad/clients/facebook/campaign.rb
@@ -1,7 +1,7 @@
 class ActiveAd::Facebook::Campaign < ActiveAd::Base
   READ_FIELDS = %i[
-    account_id ad_strategy_id adlabels bid_strategy boosted_object_id brand_lift_studies budget_rebalance_flag budget_remaining buying_type
-    can_create_brand_lift_study can_use_spend_cap configured_status created_time daily_budget effective_status id is_skadnetwork_attribution issues_info
+    id account_id ad_strategy_id adlabels bid_strategy boosted_object_id brand_lift_studies budget_rebalance_flag budget_remaining buying_type
+    can_create_brand_lift_study can_use_spend_cap configured_status created_time daily_budget effective_status is_skadnetwork_attribution issues_info
     last_budget_toggling_time lifetime_budget name objective pacing_type promoted_object recommendations smart_promotion_type source_campaign source_campaign_id
     special_ad_categories special_ad_category special_ad_category_country spend_cap start_time status stop_time topline_id updated_time
   ].freeze
@@ -19,6 +19,9 @@ class ActiveAd::Facebook::Campaign < ActiveAd::Base
   alias_attribute :adlabels, :ad_labels
   alias_attribute :created_time, :created_at
   alias_attribute :updated_time, :updated_at
+  alias_attribute :last_budget_toggling_time, :last_budget_toggling_at
+  alias_attribute :start_time, :start_at
+  alias_attribute :stop_time, :stop_at
 
   # ActiveAd object attributes.
   attribute :account_id, :big_integer
@@ -38,23 +41,23 @@ class ActiveAd::Facebook::Campaign < ActiveAd::Base
   attribute :effective_status, :string
   attribute :is_skadnetwork_attribution, :boolean
   attribute :issues_info, array: true
-  attribute :last_budget_toggling_time, :datetime
+  attribute :last_budget_toggling_at, :datetime
   attribute :lifetime_budget, :big_integer
   attribute :name, :string
-  attribute :objective, :string # , default: 'LINK_CLICKS'
+  attribute :objective, :string # default: 'LINK_CLICKS'
   attribute :pacing_type, array: true
   attribute :promoted_object
   attribute :recommendations, array: true
   attribute :smart_promotion_type, :string
   attribute :source_campaign
   attribute :source_campaign_id, :big_integer
-  attribute :special_ad_categories, array: true # , default: []
+  attribute :special_ad_categories, array: true # default: []
   attribute :special_ad_category, :string
-  attribute :special_ad_category_country, array: true # , default: []
+  attribute :special_ad_category_country, array: true # default: []
   attribute :spend_cap, :big_integer
-  attribute :start_time, :datetime
-  attribute :status, :string # , default: 'PAUSED'
-  attribute :stop_time, :datetime
+  attribute :start_at, :datetime
+  attribute :status, :string # default: 'PAUSED'
+  attribute :stop_at, :datetime
   attribute :topline_id, :integer
   attribute :updated_at, :datetime
 
