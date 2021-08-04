8fd0cbaf3d0c697a0d2e0932ed044bc70915c2ff
Kobus Joubert <kobus51@yahoo.com>
2021-08-04 15:58:33 +0200

    feat: validates_inclusion_of on Facebook campaign enums

--- lib/active_ad/clients/facebook/campaign.rb
+++ lib/active_ad/clients/facebook/campaign.rb
@@ -6,6 +6,23 @@ class ActiveAd::Facebook::Campaign < ActiveAd::Base
     special_ad_categories special_ad_category special_ad_category_country spend_cap start_time status stop_time topline_id updated_time
   ].freeze
 
+  BID_STRATEGIES = %w[LOWEST_COST_WITHOUT_CAP LOWEST_COST_WITH_BID_CAP COST_CAP].freeze
+  BUYING_TYPES = %w[AUCTION RESERVED].freeze
+  CAMPAIGN_OPTIMIZATION_TYPES = %w[NONE ICO_ONLY].freeze
+  OBJECTIVES = %w[
+    APP_INSTALLS BRAND_AWARENESS CONVERSIONS EVENT_RESPONSES LEAD_GENERATION LINK_CLICKS LOCAL_AWARENESS MESSAGES OFFER_CLAIMS PAGE_LIKES POST_ENGAGEMENT
+    PRODUCT_CATALOG_SALES REACH STORE_VISITS VIDEO_VIEWS
+  ].freeze
+  STATUS = %w[ACTIVE PAUSED DELETED ARCHIVED].freeze
+  SPECIAL_AD_CATEGORIES = %w[NONE EMPLOYMENT HOUSING CREDIT ISSUES_ELECTIONS_POLITICS].freeze
+  SPECIAL_AD_CATEGORY_COUNTRIES = %w[
+    AD AE AF AG AI AL AM AN AO AQ AR AS AT AU AW AX AZ BA BB BD BE BF BG BH BI BJ BL BM BN BO BQ BR BS BT BV BW BY BZ CA CC CD CF CG CH CI CK CL CM CN CO CR CU
+    CV CW CX CY CZ DE DJ DK DM DO DZ EC EE EG EH ER ES ET FI FJ FK FM FO FR GA GB GD GE GF GG GH GI GL GM GN GP GQ GR GS GT GU GW GY HK HM HN HR HT HU ID IE IL
+    IM IN IO IQ IR IS IT JE JM JO JP KE KG KH KI KM KN KP KR KW KY KZ LA LB LC LI LK LR LS LT LU LV LY MA MC MD ME MF MG MH MK ML MM MN MO MP MQ MR MS MT MU MV
+    MW MX MY MZ NA NC NE NF NG NI NL NO NP NR NU NZ OM PA PE PF PG PH PK PL PM PN PR PS PT PW PY QA RE RO RS RU RW SA SB SC SD SE SG SH SI SJ SK SL SM SN SO SR
+    SS ST SV SX SY SZ TC TD TF TG TH TJ TK TL TM TN TO TR TT TV TW TZ UA UG UM US UY UZ VA VC VE VG VI VN VU WF WS XK YE YT ZA ZM ZW
+  ].freeze
+
   belongs_to :account
   has_many :ad_sets
   has_many :ads
@@ -44,7 +61,6 @@ class ActiveAd::Facebook::Campaign < ActiveAd::Base
   attribute :configured_status, :string
   attribute :created_at, :datetime
   attribute :daily_budget, :big_integer
-  attribute :deep_copy, :boolean
   attribute :effective_status, :string
   attribute :execution_options, array: true
   attribute :is_skadnetwork_attribution, :boolean
@@ -58,7 +74,6 @@ class ActiveAd::Facebook::Campaign < ActiveAd::Base
   attribute :pacing_type, array: true
   attribute :promoted_object
   attribute :recommendations, array: true
-  attribute :rename_options
   attribute :smart_promotion_type, :string
   attribute :source_campaign
   attribute :source_campaign_id, :big_integer
@@ -68,7 +83,6 @@ class ActiveAd::Facebook::Campaign < ActiveAd::Base
   attribute :spend_cap, :big_integer
   attribute :start_at, :datetime
   attribute :status, :string # default: 'PAUSED'
-  attribute :status_option, :string
   attribute :stop_at, :datetime
   attribute :topline_id, :integer
   attribute :updated_at, :datetime
@@ -79,6 +93,22 @@ class ActiveAd::Facebook::Campaign < ActiveAd::Base
   # validates_length_of :title, maximum: 24
   # validates :titles, titles_length: { maximums: [24, 50] }
   validates_presence_of :name, :status, :objective, :special_ad_categories, on: :create
+
+  validates_inclusion_of :bid_strategy, in: BID_STRATEGIES, allow_blank: true, message: validates_inclusion_of_message(BID_STRATEGIES)
+  validates_inclusion_of :buying_type, in: BUYING_TYPES, allow_blank: true, message: validates_inclusion_of_message(BUYING_TYPES)
+  validates_inclusion_of :campaign_optimization_type, in: CAMPAIGN_OPTIMIZATION_TYPES,
+                                                      allow_blank: true,
+                                                      message: validates_inclusion_of_message(CAMPAIGN_OPTIMIZATION_TYPES)
+  validates_inclusion_of :objective, in: OBJECTIVES, allow_blank: true, message: validates_inclusion_of_message(OBJECTIVES)
+  validates_inclusion_of :status, in: STATUS, on: :create, message: validates_inclusion_of_message(STATUS)
+  validates_inclusion_of :special_ad_categories, in: SPECIAL_AD_CATEGORIES, on: :create, message: validates_inclusion_of_message(SPECIAL_AD_CATEGORIES)
+  validates_inclusion_of :special_ad_category, in: SPECIAL_AD_CATEGORIES,
+                                               on: :update,
+                                               allow_blank: true,
+                                               message: validates_inclusion_of_message(SPECIAL_AD_CATEGORIES)
+  validates_inclusion_of :special_ad_category_country, in: SPECIAL_AD_CATEGORY_COUNTRIES,
+                                                       allow_blank: true,
+                                                       message: validates_inclusion_of_message(SPECIAL_AD_CATEGORY_COUNTRIES)
 
   # Use callbacks to execute code that should happen before or after `create`, `update`, `save` or `destroy`.
   #
